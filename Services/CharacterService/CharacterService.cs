db72193d6f8a0ab261f51e0647086c48ff92c518
Maurício de Carvalho <mauriciocr223@gmail.com>
2021-08-26 19:32:40 -0300

    feat: Put implementations

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -74,7 +74,7 @@ namespace dotnet_rpg.Services.CharacterService
       var serviceResponse = new ServiceResponse<GetCharacterDto>();
       try
       {
-        Character character = characters.FirstOrDefault(c => c.Id == updatedCharacter.Id);
+        Character character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
 
         character.Name = updatedCharacter.Name;
         character.HitPoint = updatedCharacter.HitPoint;
@@ -82,6 +82,8 @@ namespace dotnet_rpg.Services.CharacterService
         character.Defense = updatedCharacter.Defense;
         character.Intelligence = updatedCharacter.Intelligence;
         character.Class = updatedCharacter.Class;
+
+        await _context.SaveChangesAsync();
 
         serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
       }
